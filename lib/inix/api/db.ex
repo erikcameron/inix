db8f37eee34556e9ebd2aa881ce6356f169dcf5a
Erik Cameron <erik.cameron@gnar.dog>
2024-04-30 13:50:58 -0500

    db is out of scope

--- lib/inix/api/db.ex
+++
@@ -1,5 +0,0 @@
-defmodule Inix.Api.Db do
-  @moduledoc """
-  The interface to the system database.
-  """
-end
--- lib/inix/system/db.ex
+++
@@ -1,5 +0,0 @@
-defmodule Inix.System.Db do
-  @moduledoc """
-  The system database, presumably Postgres.
-  """
-end
